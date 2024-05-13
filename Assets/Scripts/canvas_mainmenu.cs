65edd3f30514959c440dc078bed18d5501dc15b9
Neo Te Aika <neo.teaika@gmail.com>
2024-05-14 00:55:19 +1200

    craaaazy! ui mostly "done"

--- Assets/Scripts/canvas_mainmenu.cs
+++ Assets/Scripts/canvas_mainmenu.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 
 public class canvas_mainmenu : MonoBehaviour
 {
-    public GameObject MenuMain;
+    // Top Level Items
+    public GameObject MenuMain; //  Spinning Gloo intro thingy, from here you can play, quit or view the credits
     public GameObject MenuPlay;
     public GameObject MenuExit;
+    public GameObject MenuAbout;
+
+    // Play Menu Items
+    public GameObject MenuLevels;
+    public GameObject MenuCostumes;
+    public GameObject MenuMuseum;
     public GameObject MenuSetup;
-    // Start is called before the first frame update
+    public GameObject MenuHelp;
+
+    // Setup Menu Items
+    //public GameObject AudioSFX;
+    //public GameObject AudioMusic;
+    //public GameObject DisplayFullscreen;
+    //public GameObject GoBack_Setup;
+
     void Start()
     {
         ButtonMain();
     }
+    public void ButtonLevels()
+    {
+        MenuHelp.SetActive(false);
+        MenuLevels.SetActive(true);
 
+        MenuMain.SetActive(false);
+        MenuPlay.SetActive(false);
+        MenuExit.SetActive(false);
+        MenuAbout.SetActive(false);
+    }
     public void ButtonLaunch()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("TestProBuild");
         Debug.Log("LoadScene");
+    }
+     public void ButtonHelp()
+    {
+        MenuHelp.SetActive(true);
+        MenuLevels.SetActive(false);
+
+        MenuMain.SetActive(false);
+        MenuPlay.SetActive(false);
+        MenuExit.SetActive(false);
+        MenuAbout.SetActive(false);
     }
     public void ButtonExitConfirm()
     {
@@ -25,32 +58,44 @@ public class canvas_mainmenu : MonoBehaviour
     }
     public void ButtonMain()
     {
+        MenuHelp.SetActive(false);
+        MenuLevels.SetActive(false);
+
         MenuMain.SetActive(true);
         MenuPlay.SetActive(false);
         MenuExit.SetActive(false);
-        MenuSetup.SetActive(false);
+        MenuAbout.SetActive(false);
     }
     public void ButtonPlay()
     {
+        MenuHelp.SetActive(false);
+        MenuLevels.SetActive(false);
+        
         MenuMain.SetActive(false);
         MenuPlay.SetActive(true);
         MenuExit.SetActive(false);
-        MenuSetup.SetActive(false);
+        MenuAbout.SetActive(false);
     }
 
-    public void ButtonSetup()
+    public void ButtonAbout()
     {
+        MenuHelp.SetActive(false);
+        MenuLevels.SetActive(false);
+        
         MenuMain.SetActive(false);
         MenuPlay.SetActive(false);
         MenuExit.SetActive(false);
-        MenuSetup.SetActive(true);
+        MenuAbout.SetActive(true);
     }
 
         public void ButtonExit()
     {
+        MenuHelp.SetActive(false);
+        MenuLevels.SetActive(false);
+        
         MenuMain.SetActive(false);
         MenuPlay.SetActive(false);
         MenuExit.SetActive(true);
-        MenuSetup.SetActive(false);
+        MenuAbout.SetActive(false);
     }
 }
